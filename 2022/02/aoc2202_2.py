9dba4609deca5332e7bb66337c4437d60054d9f6
Dietrich Raisin <info1@raisin.de>
2022-12-03 15:55:24 +0100

    aoc2202 done

---
+++ 2022/02/aoc2202_2.py
@@ -0,0 +1,34 @@
+# https://adventofcode.com/2022/day/01
+from pathlib import Path
+
+ref = 0
+part = "_1"
+
+ext = "_ref" + str(ref) + ".txt" if ref else ".txt"
+path = Path(__file__).parent.absolute()
+input = open(path/("input" + ext), "r").read().rstrip().split("\n")
+# ---
+
+rps = [
+    0+3, 3+1, 6+2,
+    0+1, 3+2, 6+3,
+    0+2, 3+3, 6+1,
+]
+
+
+def run():
+    inElf = {'A': 0, 'B': 1, 'C': 2}
+    inMe = {'X': 0, 'Y': 1, 'Z': 2}
+    sum = 0
+    for i in input:
+        elf = inElf[i[0]]
+        me = inMe[i[2]]
+        sum += rps[elf*3+me]
+
+    return sum
+
+
+# ---
+result = run()
+print(result)
+open(path/("result" + part + ext), "w").write(str(result).rstrip() + "\n")
