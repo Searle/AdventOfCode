a8078ceeda7b0adcf9c52da2f0c361676395a784
Dietrich Raisin <info1@raisin.de>
2022-12-08 19:59:18 +0100

    aoc2002 done

---
+++ 2020/02/aoc2002_1.py
@@ -0,0 +1,31 @@
+# https://adventofcode.com/2020/day/02
+from pathlib import Path
+import re
+from functools import reduce
+
+ref = 0
+part = "_1"
+
+ext = "_ref" + str(ref) + ".txt" if ref else ".txt"
+path = Path(__file__).parent.absolute()
+inputs = open(path/("input" + ext), "r").read().rstrip().split("\n")
+# ---
+
+
+def run():
+
+    result = 0
+    for input in inputs:
+        m = re.match(r'^(\d+)-(\d+) (\S): (\S+)', input)
+        if m:
+            n = m[4].count(m[3])
+            if n >= int(m[1]) and n <= int(m[2]):
+                result += 1
+
+    return result
+
+
+# ---
+result = run()
+print(result)
+open(path/("result" + part + ext), "w").write(str(result).rstrip() + "\n")
---
+++ 2020/02/aoc2002_2.py
@@ -0,0 +1,30 @@
+# https://adventofcode.com/2020/day/02
+from pathlib import Path
+import re
+from functools import reduce
+
+ref = 0
+part = "_2"
+
+ext = "_ref" + str(ref) + ".txt" if ref else ".txt"
+path = Path(__file__).parent.absolute()
+inputs = open(path/("input" + ext), "r").read().rstrip().split("\n")
+# ---
+
+
+def run():
+
+    result = 0
+    for input in inputs:
+        m = re.match(r'^(\d+)-(\d+) (\S): (\S+)', input)
+        if m:
+            if (m[4][int(m[1]) - 1] == m[3]) != (m[4][int(m[2]) - 1] == m[3]):
+                result += 1
+
+    return result
+
+
+# ---
+result = run()
+print(result)
+open(path/("result" + part + ext), "w").write(str(result).rstrip() + "\n")
