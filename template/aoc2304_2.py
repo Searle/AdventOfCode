053638f6e1487f2891b946e62912b40eccb9002a
Dietrich Raisin <info1@raisin.de>
2023-12-04 12:42:22 +0100

    Add template

---
+++ template/aoc2304_2.py
@@ -0,0 +1,28 @@
+# https://adventofcode.com/2023/day/04
+
+from pathlib import Path
+import re
+from typing import List
+
+ref = 1
+part = "_2"
+
+ext = "_ref" + str(ref) + ".txt" if ref else ".txt"
+path = Path(__file__).parent.absolute()
+inputs = open(path / ("input" + ext), "r").read().rstrip().split("\n")
+# ---
+
+
+def run():
+    result = 0
+
+    for input in inputs:
+        pass
+
+    return result
+
+
+# ---
+result = run()
+print(result)
+open(path / ("result" + part + ext), "w").write(str(result).rstrip() + "\n")
